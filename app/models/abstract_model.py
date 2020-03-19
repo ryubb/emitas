f30ef39c3008f4155ddbfc06c317f3df61d4d785
橋本龍之介 <ryubb1108@gmail.com>
2020-03-19 23:15:44 +0900

    feat:ユーザーモデルを作成

---
+++ app/models/abstract_model.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .manager.abstract_manager import AbstractManager
+
+
+class AbstractModel(models.Model):  # 全モデル共通の処理を実装する場合に、機能を実装するクラス
+    created_at = models.DateTimeField(
+        null=False, auto_now_add=True, verbose_name="作成日時")
+    updated_at = models.DateTimeField(
+        null=False, auto_now=True, verbose_name="更新日時")
+
+    class Meta:
+        abstract = True
+
+    objects = AbstractManager()
---
+++ app/models/manager/user_manager.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+# 下記の記事を参考（ほぼまるパクリ）
+# https://qiita.com/okoppe8/items/10ae61808dc3056f9c8e
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
